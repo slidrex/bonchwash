cfed82cee0f6ae9eccd9b865a45d417a5a867c3e
Ovsenka <ovsenka3@gmail.com>
2024-09-29 15:57:41 +0300

    fix structure

--- src/repositories/manage_repository.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-def get_db_path():
-    # Assuming the same relative path logic
-    base_dir = os.path.dirname(__file__)  # Get the current file's directory
-    return os.path.join(base_dir, "..", "..", "laundry.db")
