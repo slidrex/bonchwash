07aa6055764a63ebc3dc0c98ee4d0a1dd1b04f83
srmrkN <maks.yarcheev@mail.ru>
2024-10-01 19:02:06 +0300

    Compromised adaptivity; desktop view remains functional

---
+++ frontend/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="BonchWash">
+    <BookingPage />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import BookingPage from './components/BookingPage.vue';  // Импорт компонента страницы бронирования
+
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    BookingPage,
+  },
+});
+</script>
+
+<style scoped>
+
+</style>
