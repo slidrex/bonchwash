30455fb1ecce47a8164338c4017049ac6b1853f7
slidrex <superpes3000@gmail.com>
2024-11-08 21:57:24 +0300

    back uri revert

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import BasicCredentialsPage from "./components/BookingPage/BasicCredentialsPage";
-//import BookingPage from "./components/BookingPage/BookingPage";
+import BookingPage from "./components/BookingPage/BookingPage";
 import AuthPage from "./components/BookingPage/AuthPage";
 
 function App() {
@@ -9,8 +9,9 @@ function App() {
         <Router>
             <div className="App">
                 <Routes>
+                    <Route path="/auth" element={<AuthPage />} />
                     <Route path="/credentials" element={<BasicCredentialsPage />} />
-                    <Route path="/booking" element={<AuthPage />} />
+                    <Route path="/booking" element={<BookingPage />} />
                 </Routes>
             </div>
         </Router>
