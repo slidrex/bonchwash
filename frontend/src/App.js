2204ec46c6694c1efed204c838ae3318de66afcb
slidrex <superpes3000@gmail.com>
2024-11-08 21:56:38 +0300

    test auth page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import BasicCredentialsPage from "./components/BookingPage/BasicCredentialsPage";
-import BookingPage from "./components/BookingPage/BookingPage";
+//import BookingPage from "./components/BookingPage/BookingPage";
+import AuthPage from "./components/BookingPage/AuthPage";
 
 function App() {
     return (
         <Router>
             <div className="App">
                 <Routes>
-                    <Route path="/auth" element={<BookingPage />} />
                     <Route path="/credentials" element={<BasicCredentialsPage />} />
-                    <Route path="/booking" element={<BookingPage />} />
+                    <Route path="/booking" element={<AuthPage />} />
                 </Routes>
             </div>
         </Router>
