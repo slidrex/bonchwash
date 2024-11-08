1bb9bbfecd76295b627b8438979b4e1422d7fb5c
slidrex <superpes3000@gmail.com>
2024-11-08 21:51:04 +0300

    test auth page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,13 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import BasicCredentialsPage from "./components/BookingPage/BasicCredentialsPage";
 import BookingPage from "./components/BookingPage/BookingPage";
-import VKIDButton from "./components/VKIDButton";
 
 function App() {
     return (
         <Router>
             <div className="App">
                 <Routes>
-                    { <Route path="/" element={<VKIDButton />} />   }
+                    <Route path="/auth" element={<BookingPage />} />
                     <Route path="/credentials" element={<BasicCredentialsPage />} />
                     <Route path="/booking" element={<BookingPage />} />
                 </Routes>
