0aa624b306b7717be6ddd250e21e83f67d0857b5
slidrex <superpes3000@gmail.com>
2024-11-16 17:49:25 +0300

    auth vkid init remove

--- frontend/src/components/VKIDButton.js
+++ frontend/src/components/VKIDButton.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { redirect } from "react-router-dom";
 
 function VKIDButton() {
+    const vkidContainerRef = useRef(null);
 
-
+    useEffect(() => {
+        // Проверяем, был ли уже добавлен скрипт VKID SDK
         if (!window.VKIDSDK) {
+            console.log("run")
             const script = document.createElement('script');
             script.src = 'https://unpkg.com/@vkid/sdk@<3.0.0/dist-sdk/umd/index.js';
             script.async = true;
@@ -15,13 +19,14 @@ function VKIDButton() {
                     VKID.Config.init({
                         app: 52503899,
                         redirectUrl: 'https://bonchwash.ru',
-                        state: '3jkhri0fvh38v34b9p33hvjhn'
+                        responseMode: VKID.ConfigResponseMode.Callback,
+                        source: VKID.ConfigSource.LOWCODE,
                     });
 
                     const oneTap = new VKID.OneTap();
 
                     oneTap.render({
-                        container: document.getElementById('VkIdSdkOneTap'),
+                        container: vkidContainerRef.current,
                         fastAuthEnabled: false,
                         showAlternativeLogin: true,
                         styles: {
@@ -30,10 +35,12 @@ function VKIDButton() {
                             height: 40,
                         },
                     })
+                        .on(VKID.WidgetEvents.ERROR, vkidOnError)
                         .on(VKID.OneTapInternalEvents.LOGIN_SUCCESS, function (payload) {
+                            const { code, device_id: deviceId } = payload;
 
 
-                            // Отправка запроса для обмена кода на токены
+
                             fetch("https://bonchwash.ru/api/v1/exchange-code", {
                                 method: "POST",
                                 headers: {
@@ -48,11 +55,14 @@ function VKIDButton() {
                                 .then(response => response.json())
                                 .then(data => {
                                     console.log("Токены:", data);
+                                    vkidOnSuccess(data)
                                 })
                                 .catch(error => console.error("Ошибка обмена кода:", error));
-                        })
-                        .on(VKID.WidgetEvents.ERROR, error => {
-                            console.error("Ошибка авторизации VK:", error);
+
+
+
+
+
                         });
                 }
             };
@@ -63,8 +73,38 @@ function VKIDButton() {
                 document.body.removeChild(script);
             };
         }
+    }, []);
 
-    return <div  />;
+    function vkidOnSuccess(data) {
+        console.log('Авторизация успешна:', data);
+        fetch('https://bonchwash.ru/api/v1/auth', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: 'include',
+            body: JSON.stringify({
+                access_token: data.access_token,
+                refresh_token: data.refresh_token,
+                vk_user_id: data.user_id,
+            })
+        })
+            .then(response => {
+                if (!response.ok) throw new Error('Authentication failed');
+                return response.json();
+            })
+            .then(data => {
+                console.log('Authentication successful:', data);
+                return redirect("/booking");
+            })
+            .catch(error => console.error('Auth Error:', error));
+    }
+
+    function vkidOnError(error) {
+        console.error('Ошибка авторизации:', error);
+    }
+
+    return <div ref={vkidContainerRef} />;
 }
 
 export default VKIDButton;
